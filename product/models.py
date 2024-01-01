2ee915b2a2ea4cb2f94041e42c2f67231d3708f3
Hooman.sp <1380hz1380hz@gmail.com>
2024-01-01 18:06:00 +0330

    add model product and setup settings.py and add admin register for admin panel

--- product/models.py
+++ product/models.py
@@ -6,16 +6,40 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=226, verbose_name="نام")
     parent_category = models.ForeignKey(to="self", on_delete=models.CASCADE, null=True, blank=True, editable=True)
-    slug = models.SlugField(max_length=500, verbose_name="", unique=True)
-    image = models.ImageField(upload_to="image/category", null=True, blank=True, verbose_nam="")
+    slug = models.SlugField(max_length=500, verbose_name="آدرس اینترنتی", unique=True)
+    image = models.ImageField(upload_to="image/category", null=True, blank=True, verbose_name="تصویر")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+
+    def __str__(self):
+        return f'{self.name} - {self.slug}'
+    class Meta:
+        verbose_name = 'دسته بندی محصول'
+        verbose_name_plural = 'دسته بندی محصولات'
+
 
 
 class Product(models.Model):
     name = models.CharField(max_length=226, verbose_name="نام محصول")
-    category = models.ManyToManyField(to=ProductCategory, verbose_name="", related_name="category_back")
+    category = models.ManyToManyField(to=ProductCategory, verbose_name="دسته بندی", related_name="category_back")
     description = models.TextField(max_length=7000, verbose_name="توضیحات")
-    image = models.ImageField(upload_to="image/product", null=True, blank=True)
     # price = models.IntegerField(verbose_name="قیمت", null=True, blank=True)
     slug = models.SlugField(unique=True, verbose_name="آدرس اینترنتی", max_length=500, db_index=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
 
+    def __str__(self):
+        return f'{self.name} - {self.slug}'
 
+    class Meta:
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name="محصول", related_name="product_back")
+    image = models.ImageField(upload_to="image/product_gallery", verbose_name="تصویر")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+
+    def __str__(self):
+        return f'{self.product} - {self.created}'
+    class Meta:
+        verbose_name = 'گالری محصول'
+        verbose_name_plural = 'گالری محصولات'
--- vitamol/settings.py
+++ vitamol/settings.py
@@ -30,14 +30,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
 
 LOCAL_APPS = [
     "product.apps.ProductConfig",
@@ -48,6 +40,17 @@ EXTERNAL_APPS = [
     # "rest_framework_simplejwt",
     "drf_spectacular",
     # "django_celery_beat",
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    *EXTERNAL_APPS,
+    *LOCAL_APPS,
 ]
 
 MIDDLEWARE = [
@@ -127,7 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
