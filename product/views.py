db38d3efcdca19107ac8a192166205766aa16758
Hooman.sp <1380hz1380hz@gmail.com>
2024-01-02 20:55:50 +0330

    add view.py product(whole)

--- product/views.py
+++ product/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Product,ProductGallery,ProductCategory
+from .serializers import ProductSerializer, ProductCategorySerializer, ProductGallerySerializer
+
+
+# Product
+class ProductListView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+class ProductDetailView(RetrieveAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    lookup_field = 'slug'
+
+# productCategory
+class ProductCategoryListView(ListAPIView):
+    serializer_class = ProductCategorySerializer
+    queryset = ProductCategory.objects.all()
+
+class ProductCategoryDetailView(RetrieveAPIView):
+    serializer_class = ProductCategorySerializer
+    queryset = ProductCategory.objects.all()
+    lookup_field = 'slug'
+
+class ProductGalleryDetailView(RetrieveAPIView):
+    serializer_class = ProductGallerySerializer
+    queryset = ProductGallery.objects.all()
