aba8521eb4af4a9555002ea1e04ca7d21a3b4c1d
Hooman.sp <1380hz1380hz@gmail.com>
2024-01-18 20:49:17 +0330

    add hiring model

--- site_settings/models.py
+++ site_settings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+# hiring
+
+class Hiring(models.Model):
+    fullname = models.CharField(max_length=300,verbose_name="نام و نام خانوادگی")
+    phone = models.CharField(max_length=13,verbose_name="شماره تماس",db_index=True)
+    email = models.EmailField(max_length=150,verbose_name="ایمیل",null=True,blank=True)
+    description = models.TextField(max_length=1500,verbose_name="توضیحات")
+    pdf_file = models.FileField(upload_to='hiring',verbose_name="فایل PDF")
+
+    def __str__(self):
+        return f"{self.fullname} - {self.phone}"
+
+    class Meta:
+        verbose_name = "بخش استخدام"
+        verbose_name_plural = "بخش استخدامی ها"
