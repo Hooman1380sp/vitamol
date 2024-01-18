7c547a9eebba84069f2fa7fa97f0a3338e9504e5
Hooman.sp <1380hz1380hz@gmail.com>
2024-01-18 13:59:08 +0330

    change in serializers.py (product)

--- product/serializers.py
+++ product/serializers.py
@@ -1,23 +1,6 @@
 from rest_framework import serializers
 from .models import Product, ProductGallery, ProductCategory
 
-
-class ProductSerializerList(serializers.ModelSerializer):
-    images = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Product
-        fields = ("name", "category", "description", "images")
-
-    def get_description(self, obj):
-        return obj.description[:50]
-
-    def get_images(self, obj):
-        result = obj.gallery.all()
-        return ProductGallerySerializer(instance=result, many=True).data
-
-
 class ProductSerializerDetail(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
 
