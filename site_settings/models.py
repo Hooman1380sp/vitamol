8888b0f32f79ab387d0bdadd59866921b8ce7b62
Hooman.sp <1380hz1380hz@gmail.com>
2024-01-22 21:44:25 +0330

    day field(int type) become to string type

--- site_settings/models.py
+++ site_settings/models.py
@@ -28,7 +28,7 @@ class ContactUs(models.Model):
     fullname = models.CharField(max_length=300, verbose_name="نام و نام خانوادگی")
     phone = models.CharField(max_length=13, verbose_name="شماره تماس", db_index=True)
     email = models.EmailField(max_length=150, verbose_name="ایمیل", null=True, blank=True)
-    day = models.PositiveSmallIntegerField(verbose_name="روز هفته")
+    day = models.CharField(max_length=40,verbose_name="روز هفته")
     hour = models.CharField(max_length=40, verbose_name="ساعت")
 
     def __str__(self):
